8e1d503eded91f56041b19a5f50f5bd092a991b1
Jonathan De Alba <jdealba15@yahoo.com>
2024-09-18 12:45:22 -0700

    test rejection

--- sum.test.js
+++ sum.test.js
@@ -2,4 +2,8 @@ const fetchPromise = require('./sum')
 
 test('the data is peanut butter', () => {
     return expect(fetchPromise()).resolve.toBe('peanut butter');
-});
+}); 
+
+test('the test fails with an error', () => {
+    return expect(fetchPromise()).rejects.toThrow('error');
+})
