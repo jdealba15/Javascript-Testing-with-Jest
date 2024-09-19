ef8bb7f22dd6818eb3a1c130ee3546f741a76923
Jonathan De Alba <jdealba15@yahoo.com>
2024-09-19 10:49:40 -0700

    testing asynchronous functions

--- sum.test.js
+++ sum.test.js
@@ -1,9 +1,6 @@
 const fetchPromise = require('./sum')
 
-test('the data is peanut butter', () => {
-    return expect(fetchPromise()).resolves.toBe('peanut butter');
-});
-
-test('the fetch fails with an error', () => {
-    return expect(fetchPromise()).rejects.toThrow('error');
+test('the data is peanut butter', async () => {
+    const data = await fetchPromise();
+    expect(data).toBe('peanut butter');
 });
