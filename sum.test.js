3e26033868361eb59661d62ad6fc5a004fe9f028
Jonathan De Alba <jdealba15@yahoo.com>
2024-09-19 20:53:38 -0700

    Mock Functions in Jest

--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,5 @@
-const fetchPromise = require('./sum')
-
-test('the data is peanut butter', async () => {
-    const data = await fetchPromise();
-    expect(data).toBe('peanut butter');
-});
+test('mock implementation of a basic function', () => {
+    const mock = jest.fn(x => 42 + x);
+    expect(mock(1)).toBe(43);
+    expect(mock).toHaveBeenCalledWith(1);
+})
